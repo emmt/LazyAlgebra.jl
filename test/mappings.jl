f27e8a7328e9248f542796d5068cd99021c58950
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-04-02 18:40:56 +0200

    More tests

--- test/mappings.jl
+++ test/mappings.jl
@@ -25,20 +25,52 @@ const I = LazyAlgebra.Identity()
         @test approxsame(n, C'*x, sum(w.*x)*w)
     end
 
+    @testset "Uniform scaling ($T)" for T in (Float16, Float32, Float64)
+        x = randn(T, dims)
+        y = randn(T, dims)
+        γ = sqrt(2)
+        ϵ = eps(T)
+        U = UniformScalingOperator(γ)
+        @test approxsame(2, U*x, γ*x)
+        @test approxsame(2, U'*x, γ*x)
+        @test approxsame(2, U\x, (1/γ)*x)
+        @test approxsame(2, U'\x, (1/γ)*x)
+        for α in (0, 1, -1,  2.71, π),
+            β in (0, 1, -1, -1.33, φ)
+            for P in (Direct, Adjoint)
+                @test 0 == maxrelabsdif(apply!(α, P, U, x, β, vcopy(y)),
+                                        T(α*γ)*x + T(β)*y)
+            end
+            for P in (Inverse, InverseAdjoint)
+                @test 0 == maxrelabsdif(apply!(α, P, U, x, β, vcopy(y)),
+                                        T(α/γ)*x + T(β)*y)
+            end
+        end
+    end
+
     @testset "Scaling operators ($T)" for T in (Float16, Float32, Float64)
         w = randn(T, dims)
         x = randn(T, dims)
         y = randn(T, dims)
+        wx = w.*x
+        qx = x./w
+        z = vcreate(y)
         S = NonuniformScalingOperator(w)
-        @test approxsame(2, S*x, w.*x)
-        @test approxsame(2, S'*x, w.*x)
-
-        alpha = sqrt(2)
-        U = UniformScalingOperator(alpha)
-        @test approxsame(2, U*x, alpha*x)
-        @test approxsame(2, U'*x, alpha*x)
-        @test approxsame(2, U\x, (1/alpha)*x)
-        @test approxsame(2, U'\x, (1/alpha)*x)
+        ϵ = eps(T)
+        @test approxsame(2, S*x, wx)
+        @test approxsame(2, S'*x, wx)
+        for α in (0, 1, -1,  2.71, π),
+            β in (0, 1, -1, -1.33, φ)
+            for P in (Direct, Adjoint)
+                @test 0 == maxrelabsdif(apply!(α, P, S, x, β, vcopy(y)),
+                                        T(α)*w.*x + T(β)*y)
+            end
+            for P in (Inverse, InverseAdjoint)
+                @test 0 == maxrelabsdif(apply!(α, P, S, x, β, vcopy(y)),
+                                        T(α)*x./w + T(β)*y)
+            end
+        end
     end
+
 
 end
--- test/vectors.jl
+++ test/vectors.jl
@@ -16,24 +16,18 @@
     @testset "vscale ($T)" for T in (Float16, Float32, Float64)
         a = randn(T, dims)
         b = vcreate(a)
-        α = 2.71
-        @test (0,0) == extrema(vscale!(b, 0,a) - 0*a)
-        @test (0,0) == extrema(vscale!(b, 1,a) - a)
-        @test (0,0) == extrema(vscale!(b,-1,a) + a)
-        @test (0,0) == extrema(vscale!(b, α,a) - T(α)*a)
-        @test (0,0) == extrema(vscale!(b, π,a) - T(π)*a)
+        for α in (0, -1, 1, π, 2.71)
+            @test (0,0) == extrema(vscale!(b,α,a) - T(α)*a)
+        end
     end
     @testset "vupdate ($T)" for T in (Float16, Float32, Float64)
         a = randn(T, dims)
         d = randn(T, dims)
-        b = vcreate(a)
-        α = 2.71
-        ϵ = eps(T)
-        @test (0,0) == extrema(vupdate!(vcopy!(b,a), 0,d) - a)
-        @test (0,0) == extrema(vupdate!(vcopy!(b,a), 1,d) - (a + d))
-        @test (0,0) == extrema(vupdate!(vcopy!(b,a),-1,d) - (a - d))
-        @test 10ϵ ≥ maxrelabsdif(vupdate!(vcopy!(b,a),α,d), a + T(α)*d)
-        @test 10ϵ ≥ maxrelabsdif(vupdate!(vcopy!(b,a),π,d), a + T(π)*d)
+        for α in (0, -1, 1, π, 2.71)
+            # FIXME: tighten this tolerance
+            ϵ = sqrt(eps(T))*(maximum(abs.(a)) + abs(α)*maximum(abs.(d)))
+            @test maxrelabsdif(vupdate!(vcopy(a),α,d), a + T(α)*d) ≤ ϵ
+        end
     end
     @testset "vproduct ($T)" for T in (Float16, Float32, Float64)
         a = randn(T, dims)
@@ -45,24 +39,9 @@
         a = randn(T, dims)
         b = randn(T, dims)
         d = vcreate(a)
-        α =  2.71
-        β = -1.33
-        @test (0,0) == extrema(vcombine!(d, 0,a, 0,b) - zeros(T,dims))
-        @test (0,0) == extrema(vcombine!(d, 0,a, 1,b) - b)
-        @test (0,0) == extrema(vcombine!(d, 0,a,-1,b) + b)
-        @test (0,0) == extrema(vcombine!(d, 0,a, β,b) - T(β)*b)
-        @test (0,0) == extrema(vcombine!(d, 0,a, π,b) - T(π)*b)
-        @test (0,0) == extrema(vcombine!(d, 1,a, 0,b) - a)
-        @test (0,0) == extrema(vcombine!(d,-1,a, 0,b) + a)
-        @test (0,0) == extrema(vcombine!(d, α,a, 0,b) - T(α)*a)
-        @test (0,0) == extrema(vcombine!(d, π,a, 0,b) - T(π)*a)
-        @test (0,0) == extrema(vcombine!(d, 1,a, 1,b) - (a + b))
-        @test (0,0) == extrema(vcombine!(d,-1,a, 1,b) - (b - a))
-        @test (0,0) == extrema(vcombine!(d, 1,a,-1,b) - (a - b))
-        @test (0,0) == extrema(vcombine!(d,-1,a,-1,b) + (a + b))
-        @test (0,0) == extrema(vcombine!(d, α,a, β,b) - (T(α)*a + T(β)*b))
-        @test (0,0) == extrema(vcombine!(d, α,a, φ,b) - (T(α)*a + T(φ)*b))
-        @test (0,0) == extrema(vcombine!(d, π,a, β,b) - (T(π)*a + T(β)*b))
-        @test (0,0) == extrema(vcombine!(d, π,a, φ,b) - (T(π)*a + T(φ)*b))
+        for α in (0, -1, 1, π,  2.71),
+            β in (0, -1, 1, φ, -1.33)
+            @test (0,0) == extrema(vcombine!(d,α,a,β,b) - (T(α)*a + T(β)*b))
+        end
     end
 end
