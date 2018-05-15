ca75507cdae781aebdf281a9a1c2781bec051148
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-05-15 19:01:37 +0200

    Even more tests

--- test/conjgrad.jl
+++ test/conjgrad.jl
@@ -27,10 +27,12 @@ const DEBUG = true
         x = randn(T, cols)
         y = H*x + 0.01*randn(T, rows)
 
-        # Lesat-squares solution using Julia linear algebra.
+        # Least-squares solution using Julia linear algebra.
         yj = reshape(y, length(y))
         Hj = reshape(contents(H), length(y), length(x))
-        x0 = reshape((Hj'*Hj)\(Hj'*yj), cols)
+        Aj = Hj'*Hj
+        bj = Hj'*yj
+        x0 = reshape(Aj\bj, cols)
 
         # Tolerances.
         rtol = 1e-4
@@ -55,20 +57,44 @@ const DEBUG = true
 
         # Exercise other convergence tests.
         x3 = conjgrad(A, b; maxiter=n, restart=n,
-                      quiet=false, verb=false,
+                      quiet=false, verb=true,
                       gtol=(0,0), ftol=0, xtol=0)
         DEBUG && println("x3: ", vnorm2(x3 - x0)/vnorm2(x0))
         @test vnorm2(x3 - x0) ≤ atol
         x4 = conjgrad(A, b; maxiter=10n, restart=n,
-                      quiet=true, verb=true,
+                      quiet=false, verb=true,
                       gtol=(0,0), ftol=1e-9, xtol=0)
         DEBUG && println("x4: ", vnorm2(x4 - x0)/vnorm2(x0))
         @test vnorm2(x4 - x0) ≤ atol
         x5 = conjgrad(A, b; maxiter=10n, restart=n,
-                      quiet=true, verb=false,
+                      quiet=false, verb=true,
                       gtol=(0,1e-6), ftol=0, xtol=0)
         DEBUG && println("x5: ", vnorm2(x5 - x0)/vnorm2(x0))
         @test vnorm2(x5 - x0) ≤ atol
+
+        # Directly use an array.
+        x6 = conjgrad(reshape(Aj, cols..., cols...), b;
+                      maxiter=2n, restart=n,
+                      quiet=true, verb=false,
+                      gtol=(0,0), ftol=0, xtol=rtol/10)
+        DEBUG && println("x6: ", vnorm2(x6 - x0)/vnorm2(x0))
+        @test vnorm2(x6 - x0) ≤ atol
+
+        # Force no iterations (should yield exactly the initial solution).
+        x7 = conjgrad(A, b, x0;
+                      maxiter=0, restart=n,
+                      quiet=true, verb=false,
+                      gtol=(0,0), ftol=0, xtol=0)
+        DEBUG && println("x7: ", vnorm2(x7 - x0)/vnorm2(x0))
+        @test vnorm2(x7 - x0) ≤ 0
+
+        # Force restarts.
+        x8 = conjgrad(A, b;
+                      maxiter=4n, restart=(n>>1),
+                      quiet=true, verb=false,
+                      gtol=(0,0), ftol=0, xtol=0)
+        DEBUG && println("x8: ", vnorm2(x8 - x0)/vnorm2(x0))
+        @test vnorm2(x8 - x0) ≤ atol
 
         # Exceptions.
         @test_throws LazyAlgebra.NonPositiveDefinite conjgrad(-A, b)
