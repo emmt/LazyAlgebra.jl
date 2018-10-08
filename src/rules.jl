0b1a523ab669c2b0f385c1cdbe04a514953f51e8
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-10-08 13:29:59 +0200

    Fix header comment

--- src/rules.jl
+++ src/rules.jl
@@ -5,8 +5,8 @@
 #
 #-------------------------------------------------------------------------------
 #
-# This file is part of the LazyAlgebra (https://github.com/emmt/LazyAlgebra.jl)
-# package released under the MIT "Expat" license.
+# This file is part of LazyAlgebra (https://github.com/emmt/LazyAlgebra.jl)
+# released under the MIT "Expat" license.
 #
 # Copyright (c) 2017-2018 Éric Thiébaut.
 #
