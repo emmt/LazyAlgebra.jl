5247c4f8f3d206d987de157ad86e67c1eb9c62aa
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-10-15 22:15:54 +0200

    Fix doc.

--- src/lgemm.jl
+++ src/lgemm.jl
@@ -20,7 +20,7 @@ lgemm([α=1,] [transA='N',] A, [transB='N',] B, Nc=2) -> C
 
 yields `C = α*op(A)*op(B)` where `op(A)` is `A` if `transA` is `'N'`,
 `transpose(A)` if `transA` is `'T'` and `adjoint(A)` if `transA` is `'C'` and
-simlarly for `op(B)` and `transB`.  The expression `op(A)*op(B)` is a
+similarly for `op(B)` and `transB`.  The expression `op(A)*op(B)` is a
 matrix-matrix multiplication but interpreted by grouping consecutive dimensions
 of `op(A)` and `op(B)` (see [`lgemv`](@ref) for explanations).  Argument `Nc`
 specifies the number of dimensions of the result.
@@ -31,8 +31,8 @@ The in-place version is called by:
 lgemm!([α=1,] [transA='N',] A,  [transB='N',] B, [β=0,] C) -> C
 ```
 
-and overwrites the contents of `C` with `α*op(A)*op(B)`.  Note that `C` must
-not be aliased with `A` or `B`.
+and overwrites the contents of `C` with `α*op(A)*op(B) + β*C`.  Note that `C`
+must not be aliased with `A` nor `B`.
 
 The multipliers `α` and `β` must be both specified or omitted, they can be any
 scalar numbers but are respectively converted to
