fe60bc6e13e0871fd8e2765f98c62d1dd695aa21
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-01-13 12:20:55 +0100

    Test adjoint of circulant convolution operator

--- test/fft-test.jl
+++ test/fft-test.jl
@@ -30,19 +30,20 @@ let FLOATS = (Float32, Float64),
 
     @testset "FFT operator ($T)" for T in TYPES
         R = real(T)
-        ϵ = 2*eps(R) # relative tolerance
+        ϵ = sqrt(eps(R)) # relative tolerance, can certainly be much tighter
         for dims in ((45,), (20,), (33,12), (30,20), (4,5,6))
             # Create a FFT operator given its argument.
             x = rand(T, dims)
+            n = length(x)
             xsav = vcopy(x)
             F = FFTOperator(x)
             @test x == xsav # check that input has been preserved
             @test MorphismType(F) == (T<:Complex ? Endomorphism() : Morphism())
             @test input_size(F) == dims
-            @test input_size(F) == ntuple(d->input_size(F,d), ndims(x))
+            @test input_size(F) == ntuple(i->input_size(F,i), ndims(x))
             @test output_size(F) == (T<:Complex ? dims :
                                      Tuple(AbstractFFTs.rfft_output_size(x, 1:ndims(x))))
-            @test output_size(F) == ntuple(d->output_size(F,d), ndims(x))
+            @test output_size(F) == ntuple(i->output_size(F,i), ndims(x))
             @test input_eltype(F) == T
             @test output_eltype(F) == typeof(complex(zero(R)))
             @test input_ndims(F) == ndims(x)
@@ -58,12 +59,18 @@ let FLOATS = (Float32, Float64),
             io = IOBuffer()
             show(io, F)
             @test String(take!(io)) == "FFT"
+            @test typeof(F') === Adjoint{typeof(F)}
+            @test typeof(inv(F)) === Inverse{typeof(F)}
+            @test typeof(inv(F)') === InverseAdjoint{typeof(F)}
+            @test typeof(inv(F')) === InverseAdjoint{typeof(F)}
             @test F'*F == length(x)*Identity()
             @test F*F' == length(x)*Identity()
             @test inv(F)*F == Identity()
             @test F*inv(F) == Identity()
-            @test inv(F)'*F' == Identity()
-            @test F'*inv(F)' == Identity()
+            @test inv(F')*F' == Identity()
+            @test F'*inv(F') == Identity()
+            @test inv(F')*inv(F) == (1//length(x))*Identity()
+            @test inv(F)*inv(F') == (1//length(x))*Identity()
 
             # Create operators which should be considered as the same as F.
             F1 =  FFTOperator(T, dims...)
@@ -72,6 +79,8 @@ let FLOATS = (Float32, Float64),
             @test LazyAlgebra.is_same_mapping(F2, F) == true
 
             # Check applying operator.
+            xbad = rand(T, ntuple(i -> (i == 1 ? dims[i]+1 : dims[i]), length(dims)))
+            @test_throws DimensionMismatch F*xbad
             y = rand(Complex{R}, (T<:Complex ? input_size(F) :
                                   output_size(F)))
             z = (T<:Complex ? fft(x) : rfft(x))
@@ -93,6 +102,13 @@ let FLOATS = (Float32, Float64),
                 else
                     @test x == xsav # check that input has been preserved
                 end
+                @test apply!(α, Adjoint, F, y, scratch, β, vcopy(x)) ≈
+                    R(n*α)*w + R(β)*x atol=0 rtol=ϵ
+                if scratch
+                    vcopy!(y, ysav)
+                else
+                    @test y == ysav # check that input has been preserved
+                end
                 @test apply!(α, Inverse, F, y, scratch, β, vcopy(x)) ≈
                     R(α)*w + R(β)*x atol=0 rtol=ϵ
                 if scratch
@@ -100,24 +116,32 @@ let FLOATS = (Float32, Float64),
                 else
                     @test y == ysav # check that input has been preserved
                 end
+                @test apply!(α, InverseAdjoint, F, x, scratch, β, vcopy(y)) ≈
+                    R(α/n)*z + R(β)*y atol=0 rtol=ϵ
+                if scratch
+                    vcopy!(x, xsav)
+                else
+                    @test x == xsav # check that input has been preserved
+                end
             end
         end
     end
 
     @testset "Circular convolution ($T)" for T in TYPES
         R = real(T)
-        ϵ = 2*eps(R) # relative tolerance
+        ϵ = sqrt(eps(R)) # relative tolerance, can certainly be much tighter
         n1, n2, n3 = 18, 12, 4
         for dims in ((n1,), (n1,n2), (n1,n2,n3))
             # Basic methods.
             x = rand(T, dims)
+            n = length(x)
             h = rand(T, dims)
-            H = CirculantConvolution(h, flags=FFTW.ESTIMATE)
+            H = CirculantConvolution(h, shift=false, flags=FFTW.ESTIMATE)
             @test MorphismType(H) == Endomorphism()
             @test input_size(H) == dims
-            @test input_size(H) == ntuple(d->input_size(H,d), ndims(x))
+            @test input_size(H) == ntuple(i->input_size(H,i), ndims(x))
             @test output_size(H) == dims
-            @test output_size(H) == ntuple(d->output_size(H,d), ndims(x))
+            @test output_size(H) == ntuple(i->output_size(H,i), ndims(x))
             @test input_eltype(H) == T
             @test output_eltype(H) == T
             @test input_ndims(H) == ndims(x)
@@ -125,20 +149,51 @@ let FLOATS = (Float32, Float64),
             @test eltype(H) == T
             @test size(H) == (dims..., dims...)
             @test ndims(H) == 2*length(dims)
+            @test (size(H)..., 1) == ntuple(i->size(H, i), ndims(H)+1)
 
             # Test apply! method.
             F = FFTOperator(x)
             G = F\Diag(F*h)*F
+            y = rand(T, dims)
+            xsav = vcopy(x)
+            ysav = vcopy(y)
             y1 = H*x
             y2 = G*x
             y3 = (T<:Real ? real(ifft(fft(h).*fft(x)))
                   :              ifft(fft(h).*fft(x)))
+            @test y1 ≈ y2 atol=0 rtol=ϵ
             @test y1 ≈ y3 atol=0 rtol=ϵ
-            @test y2 ≈ y3 atol=0 rtol=ϵ
             if T<:Real
                 y4 = irfft(rfft(h).*rfft(x), n1)
                 @test y1 ≈ y4 atol=0 rtol=ϵ
-                @test y2 ≈ y4 atol=0 rtol=ϵ
+            end
+            z1 = H'*y
+            z2 = G'*y
+            z3 = (T<:Real ? real(ifft(conj.(fft(h)).*fft(y)))
+                  :              ifft(conj.(fft(h)).*fft(y)))
+            @test z1 ≈ z2 atol=0 rtol=ϵ
+            @test z1 ≈ z3 atol=0 rtol=ϵ
+            if T<:Real
+                z4 = irfft(conj.(rfft(h)).*rfft(y), n1)
+                @test z1 ≈ z4 atol=0 rtol=ϵ
+            end
+            for α in ALPHAS,
+                β in BETAS,
+                scratch in (false, true)
+                @test apply!(α, Direct, H, x, scratch, β, vcopy(y)) ≈
+                    R(α)*y1 + R(β)*y atol=0 rtol=ϵ
+                if scratch
+                    vcopy!(x, xsav)
+                else
+                    @test x == xsav # check that input has been preserved
+                end
+                @test apply!(α, Adjoint, H, y, scratch, β, vcopy(x)) ≈
+                    R(α)*z1 + R(β)*x atol=0 rtol=ϵ
+                if scratch
+                    vcopy!(y, ysav)
+                else
+                    @test y == ysav # check that input has been preserved
+                end
             end
         end
     end
