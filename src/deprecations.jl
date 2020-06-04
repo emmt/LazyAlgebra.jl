1f6cf2ddd404c6e0db14405fcddfa3402bb4723b
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-06-04 17:20:15 +0200

    Fix deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -5,11 +5,11 @@
            promote_multiplier(λ, T))
 @deprecate(convert_multiplier(λ::Number, T::Type, ::Type),
            promote_multiplier(λ, T))
-@deprecate(makedims(args...), dimensions(args...))
-@deprecate(densearray(args...), flatarray(args...))
-@deprecate(densevector(args...), flatarray(args...))
-@deprecate(densematrix(args...), flatarray(args...))
-@deprecate(is_flat_array(args...), isflatarray(args...))
+@deprecate(makedims(args::Integer...), to_size(args))
+@deprecate(makedims(args::Tuple{Vararg{Integer}}), to_size(args))
+@deprecate(densearray(args...), to_flat_array(args...))
+@deprecate(densevector(args...), to_flat_array(args...))
+@deprecate(densematrix(args...), to_flat_array(args...))
 @deprecate(has_oneto_axes(args...), has_standard_indexing(args...))
 @deprecate(SparseOperator(rowdims::Union{Integer,Tuple{Vararg{Integer}}},
                           coldims::Union{Integer,Tuple{Vararg{Integer}}},
