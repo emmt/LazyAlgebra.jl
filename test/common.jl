e661120337e71ca1080507a02229ba4b954d61ef
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-02-28 11:46:49 +0100

    Use more readable function names

--- test/common.jl
+++ test/common.jl
@@ -7,45 +7,45 @@
 """
 
 ```julia
-floatingpointtype(A, B, ...)
+floating_point_type(A, B, ...)
 ```
 
 yields the floating-point type for operations between arrays `A`, `B`, ...
 
 """
-floatingpointtype(args::AbstractArray...) =
+floating_point_type(args::AbstractArray...) =
     float(real(promote_type(map(eltype, args)...)))
 
 """
 
 ```julia
-relativeprecision(A, B, ...)
+relative_precision(A, B, ...)
 ```
 
 yields the worst of the relative precisions of the element types of arrays `A`,
 `B`, ...
 
 """
-relativeprecision(args::AbstractArray...) = max(map(relativeprecision, args)...)
-relativeprecision(A::AbstractArray{T}) where {T} = eps(float(real(T)))
+relative_precision(args::AbstractArray...) = max(map(relative_precision, args)...)
+relative_precision(A::AbstractArray{T}) where {T} = eps(float(real(T)))
 
 """
 
 ```julia
-test_api(P, A, x, y; atol=0, rtol=sqrt(eps(relativeprecision(x,y))))
+test_api(P, A, x, y; atol=0, rtol=sqrt(eps(relative_precision(x,y))))
 ```
 
 test LazyAlgebra API for mapping `P(A)` using variables `x` and `y`.
 
 """
 function test_api(::Type{P}, A::Mapping, x0::AbstractArray, y0::AbstractArray;
-                  rtol::Real=sqrt(relativeprecision(x0,y0)),
+                  rtol::Real=sqrt(relative_precision(x0,y0)),
                   atol::Real=0) where {P<:Union{Direct,InverseAdjoint}}
     x = vcopy(x0)
     y = vcopy(y0)
     z = apply(P, A, x)
-    T = floatingpointtype(x, y, z)
     @test x == x0
+    T = floating_point_type(x, y, z)
     for α in (0, 1, -1,  2.71, π),
         β in (0, 1, -1, -1.33, Base.MathConstants.φ),
         scratch in (false, true)
@@ -60,13 +60,13 @@ function test_api(::Type{P}, A::Mapping, x0::AbstractArray, y0::AbstractArray;
 end
 
 function test_api(::Type{P}, A::Mapping, x0::AbstractArray, y0::AbstractArray;
-                  rtol::Real=sqrt(relativeprecision(x0,y0)),
+                  rtol::Real=sqrt(relative_precision(x0,y0)),
                   atol::Real=0) where {P<:Union{Adjoint,Inverse}}
     x = vcopy(x0)
     y = vcopy(y0)
     z = apply(P, A, y)
     @test y == y0
-    T = floatingpointtype(x, y, z)
+    T = floating_point_type(x, y, z)
     for α in (0, 1, -1,  2.71, π),
         β in (0, 1, -1, -1.33, Base.MathConstants.φ),
         scratch in (false, true)
