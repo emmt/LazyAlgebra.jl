dbe57e0e25cde1b70fd16636e2412c8a6e6735be
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-10-21 17:06:59 +0200

    Suppress unused functions `dimensionless` and `inplace_multiplier`

--- src/multipliers.jl
+++ src/multipliers.jl
@@ -62,24 +62,3 @@ multiplier_type(::Type{S}, ::Type{A}, ::Type{X}) where {S<:Number,A<:Operator,X<
 
 multiplier_type(::Type{S}, ::Type{T}) where {S<:Number,T<:Number} =
     convert_floating_point_type(T, S)
-
-"""
-    LazyAlgebra.inplace_multiplier(α, x) -> α′
-    LazyAlgebra.inplace_multiplier(α, eltype(x)) -> α′
-
-yield the dimensionless value of `α`, throwing an exception if `α` is not suitable for
-scaling array `x` in-place. `α` must be dimensionless, `α` may be complex only if
-`eltype(x)` is complex, and must be real otherwise. The result `α′` is a real or a complex
-value.
-
-See also [`LazyAlgebra.dimensionless`](@ref) and  [`LazyAlgebra.convert_multiplier`](@ref).
-
-"""
-inplace_multiplier(α::Number, x::AbstractArray) = inplace_multiplier(α, eltype(x))
-inplace_multiplier(α::Real, ::Type{<:Number}) = α
-inplace_multiplier(α::Complex, ::Type{<:Union{Complex,AbstractQuantity{<:Complex}}}) = α
-inplace_multiplier(α::AbstractQuantity, ::Type{x}) where {x} =
-    inplace_multiplier(dimensionless(α), x)
-@noinline inplace_multiplier(α::Number, ::Type{x}) where {x<:Number} =
-    throw(ArgumentError(
-        "multiplier of type `$(typeof(α))` is not suitable to scale in-place an array with elements of type `$x`"))
--- src/utils.jl
+++ src/utils.jl
@@ -45,35 +45,6 @@ function ordinal_suffix(n::Integer)
     return "th"
 end
 
-#------------------------------------------------------------------------- DIMENSIONLESS -
-
-"""
-    LazyAlgebra.dimensionless(x)
-
-yields the numerical value of `x` throwing an exception if `x` is dimensionful. If `x` is
-a real or complex number, `x` is returned; if `x` is a dimensionless quantity, it is
-converted to the equivalent real or complex number.
-
-Examples:
-
-```juliadoc
-julia> Using LazyAlgebra, Unitful.DefaultSymbols
-
-julia> LazyAlgebra.dimensionless(3.0)
-3.0
-
-julia> LazyAlgebra.dimensionless(8kg/g)
-8000
-
-```
-
-"""
-dimensionless(x::Real) = x
-dimensionless(x::Complex) = x
-dimensionless(x::AbstractQuantity{T,NoDims,U}) where {T,U} = uconvert(NoUnits, x)
-@noinline dimensionless(x::AbstractQuantity) =
-    throw(ArgumentError("expecting a dimensionless value, got dimensions `$(unit(x))`"))
-
 #-----------------------------------------------------------------------------------------
 
 # Yield whether a number is a real with integer storage.
